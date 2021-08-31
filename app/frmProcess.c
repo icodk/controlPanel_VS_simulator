6995a1fa152c4190d53e9557329b80dfc75e6ef9
ico <it@picatek.dk>
2021-08-31 10:55:29 +0200

    Display time/date in process form

--- app/frmProcess.c
+++ app/frmProcess.c
@@ -6,12 +6,13 @@
 
 #else
 #include <windows.h>
-#include <stdio.h>
+
 
 
 #endif
 
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "lvgl/lvgl.h"
 #include "dataStorage.h"
 #include "ui_common.h"
@@ -28,38 +29,42 @@ extern void frmMenu_init(void);
 
 static int8_t showDate= 0;
 //--------------------------------------------------
-static void resetMsgBox_event_cb(lv_event_t* e)
-{
-    lv_obj_t* obj = lv_event_get_current_target(e);
-    //LV_LOG_USER("Button %s clicked", lv_msgbox_get_active_btn_text(obj));
-    LV_LOG_USER("Event code: %d ",e->code );
-
-    if (e->code == LV_EVENT_DELETE) {
-            
-            return;
-   }
-   else { // reset was pressed
-       
-        int32_t* cVal = get_current_count((counter_id_t)(e->user_data));
-        (*cVal)=0;
-       lv_msgbox_close(obj);
-
-   }
-
-    
-
-    /*if (e->code == LV_EVENT_DELETE) {
-        msgBoxOn = false;
-    }else 
-
-    }*/
-}
+//static void resetMsgBox_event_cb(lv_event_t* e)
+//{
+//    lv_obj_t* obj = lv_event_get_current_target(e);
+//    //LV_LOG_USER("Button %s clicked", lv_msgbox_get_active_btn_text(obj));
+//    LV_LOG_USER("Event code: %d ",e->code );
+//
+//    if (e->code == LV_EVENT_DELETE) {
+//
+//            return;
+//   }
+//   else { // reset was pressed
+//
+//        int32_t* cVal = get_current_count((counter_id_t)(e->user_data));
+//        (*cVal)=0;
+//       lv_msgbox_close(obj);
+//
+//   }
+//
+//
+//
+//    /*if (e->code == LV_EVENT_DELETE) {
+//        msgBoxOn = false;
+//    }else
+//
+//    }*/
+//}
 //------------------
-static title_click_event_handler(lv_event_t * e) {
+static void  title_click_event_handler(lv_event_t * e) {
 
     if (e->code == LV_EVENT_CLICKED) {
         LV_LOG_USER("title event %d ", e->code);
-        showDate = 5;
+        if(showDate >0){
+        	showDate =0;
+        }else{
+        	showDate = 5;
+        }
     }
 
 }
@@ -75,25 +80,25 @@ static void config_init(lv_event_t* e) {
 
 //------------------
 
-static void resetMsgBox(lv_event_t* e)
-{
-    static const char* btns[] = { "Reset", "" };
- 
-    lv_obj_t* mbox1 = lv_msgbox_create(NULL, "Reset Counter", "Click Reset to reset counter.", btns, true);
-    //mbox1->user_data = 1;
-    lv_label_set_text_fmt(lv_msgbox_get_title(mbox1), "Reset Counter: %d",1+ (int)e->user_data);
-    lv_obj_add_event_cb(mbox1, resetMsgBox_event_cb, LV_EVENT_VALUE_CHANGED, e->user_data);
-    lv_obj_add_event_cb(mbox1, resetMsgBox_event_cb, LV_EVENT_DELETE/*LV_EVENT_ALL*/, e->user_data);
-    //lv_obj_add_event_cb(mbox1, msgBox_event_cb, LV_EVENT_CLICKED, NULL);
-   // lv_obj_set_style_border_width(mbox1, LV_PART_MAIN, LV_STATE_DEFAULT, 2);
-    lv_obj_set_style_border_width(mbox1, 5, LV_STATE_DEFAULT);
-    lv_obj_set_style_border_color(mbox1, lv_palette_main(LV_PALETTE_RED), LV_PART_MAIN);
-    lv_obj_center(mbox1);
-}
+//static void resetMsgBox(lv_event_t* e)
+//{
+//    static const char* btns[] = { "Reset", "" };
+// 
+//    lv_obj_t* mbox1 = lv_msgbox_create(NULL, "Reset Counter", "Click Reset to reset counter.", btns, true);
+//    //mbox1->user_data = 1;
+//    lv_label_set_text_fmt(lv_msgbox_get_title(mbox1), "Reset Counter: %d",1+ (int)e->user_data);
+//    lv_obj_add_event_cb(mbox1, resetMsgBox_event_cb, LV_EVENT_VALUE_CHANGED, e->user_data);
+//    lv_obj_add_event_cb(mbox1, resetMsgBox_event_cb, LV_EVENT_DELETE/*LV_EVENT_ALL*/, e->user_data);
+//    //lv_obj_add_event_cb(mbox1, msgBox_event_cb, LV_EVENT_CLICKED, NULL);
+//   // lv_obj_set_style_border_width(mbox1, LV_PART_MAIN, LV_STATE_DEFAULT, 2);
+//    lv_obj_set_style_border_width(mbox1, 5, LV_STATE_DEFAULT);
+//    lv_obj_set_style_border_color(mbox1, lv_palette_main(LV_PALETTE_RED), LV_PART_MAIN);
+//    lv_obj_center(mbox1);
+//}
 //------------------
 static void menu_btn_event_handler(lv_event_t * e)
 {
-    lv_obj_t * obj = lv_event_get_target(e);
+   // lv_obj_t * obj = lv_event_get_target(e);
     LV_LOG_USER("Event code %d", e->code);
     showDate = 0;
     lv_timer_t** tmr = get_updateTimer();
@@ -119,7 +124,7 @@ static void drawFrmProcess(lv_obj_t* win) {
         lv_obj_align(panel, LV_ALIGN_TOP_LEFT,5, i*50 + 5);
         lv_obj_add_style(panel, get_style_panel(), 0);
         //lv_obj_add_event_cb(panel, resetMsgBox, LV_EVENT_CLICKED, i);
-        lv_obj_add_event_cb(panel, config_init, LV_EVENT_CLICKED, i);
+        lv_obj_add_event_cb(panel, config_init, LV_EVENT_CLICKED,(void *) i);
 
         // LABEL
         lv_obj_t* label = lv_label_create(panel);
@@ -153,6 +158,29 @@ static void     updateFrmProcess(void) {
     }
 
 #ifdef __GNUC__
+    time_t now;
+    struct tm timeinfo;
+
+       time(&now);
+       // Set timezone to China Standard Time
+       setenv("TZ", "UTC+2", 1);
+       tzset();
+
+       //localtime_r(&now, &timeinfo);
+       //localtime(&now);
+       localtime_r(&now, &timeinfo);
+       if (lastSecond!=timeinfo.tm_sec ) {
+               lastSecond = timeinfo.tm_sec;
+               lv_obj_t** title = get_main_win_title();
+               if (showDate > 0) {
+                   showDate--;
+                   lv_label_set_text_fmt(*title, "%s     %02d-%02d-%02d", get_text(T_PROCESS), timeinfo.tm_year, timeinfo.tm_mon, timeinfo.tm_mday);
+               } else {
+                   lv_label_set_text_fmt(*title, "%s     %02d:%02d:%02d", get_text(T_PROCESS), timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+               }
+           }
+
+
 
 #else
     SYSTEMTIME lt;// , st;
@@ -211,19 +239,19 @@ void frmProcess_init() {
 
 #ifdef __GNUC__
 
-    time_t now;
-    char strftime_buf[64];
-    struct tm timeinfo;
-
-    time(&now);
-    // Set timezone to China Standard Time
-    //setenv("TZ", "CST-8", 1);
-    _tzset();
-
-    //localtime_r(&now, &timeinfo);
-    localtime(&now);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    printf("\n%s", strftime_buf);
+//    time_t now;
+//    char strftime_buf[64];
+//    struct tm timeinfo;
+//
+//    time(&now);
+//    // Set timezone to China Standard Time
+//    setenv("TZ", "UTC+2", 1);
+//    tzset();
+//
+//    //localtime_r(&now, &timeinfo);
+//    localtime(&now);
+//    //strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+//    printf("\n%s", strftime_buf);
 
 #else
     SYSTEMTIME st, lt;
